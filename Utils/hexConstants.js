ec4a632dba2072ac4c28a0fb2225f70633d325a3
Vedant K <82938580+dopevog@users.noreply.github.com>
2021-06-07 12:23:48 +0530

    Delete Utils directory

--- Utils/hexConstants.js
+++
@@ -1,7 +0,0 @@
-const LABEL_GRAY = '#8b8b8b';
-
-const INPUT_TEXT_GRAY = '#686868';
-
-const HOVER_GRAY = '#cecece';
-
-export { LABEL_GRAY, INPUT_TEXT_GRAY, HOVER_GRAY };
--- Utils/inputConstants.js
+++
@@ -1,5 +0,0 @@
-const MAX_SIZE = 32767;
-
-const ENTER_KEY = 13;
-
-export { MAX_SIZE, ENTER_KEY };
